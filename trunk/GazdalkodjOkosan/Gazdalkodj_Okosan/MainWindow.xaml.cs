b3b6aa6fc390f27bd9e67deecad0ca8b21bf3201
fung.alexandra@gmail.com <fung.alexandra@gmail.com@11eb12d7-8c87-527a-0a25-901ecafa439f>
2012-04-29 19:35:49 +0000

    Create Network

--- trunk/GazdalkodjOkosan/Gazdalkodj_Okosan/MainWindow.xaml.cs
+++ trunk/GazdalkodjOkosan/Gazdalkodj_Okosan/MainWindow.xaml.cs
@@ -11,6 +11,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Gazdalkodj_Okosan.Network;
+using System.Net.Sockets;
+using System.Net;
+using Gazdalkodj_Okosan.Model.Network;
+using GazdalkodjOkosan.Control;
+using System.Threading;
 
 namespace Gazdalkodj_Okosan
 {
@@ -19,9 +25,29 @@ namespace Gazdalkodj_Okosan
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //ClientController Controller;
+       // private delegate void ProcessMessageSendDelegate(MessageCode code);
+
         public MainWindow()
         {
             InitializeComponent();
+            //Controller = new ClientController();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //Controller.StartServer();
+            //Controller.BeginConnect(IPAddress.Parse("192.168.56.1"), "Startjátékos");
+            //Thread.Sleep(1000);
+            //Controller.NewGame();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            //Controller.BeginConnect(IPAddress.Parse("192.168.56.1"), "Szandi");
+            //Thread.Sleep(1000);
+            //Controller.ConnectToGame();
         }
     }
 }
---
+++ trunk/GazdalkodjOkosan/Gazdalkodj_Okosan/Model/Game/House.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GazdalkodjOkosan.Model.Game
+{
+    class House
+    {
+        private int price;
+        private int loanPrice;
+        private List<PieceOfFurniture> furniture;
+
+        public bool IsComplete()
+        {
+            return false;
+        }
+    }
+}
